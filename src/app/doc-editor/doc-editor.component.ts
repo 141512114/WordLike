7023700759eefa79cd796c8c4caff8fd3e582ad3
Nolan Schiemann <derjuniorschiemann@gmail.com>
2020-12-07 00:02:56 +0100

    initial-commit

--- src/app/doc-editor/doc-editor.component.ts
+++ src/app/doc-editor/doc-editor.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-doc-editor',
   templateUrl: './doc-editor.component.html',
   styleUrls: ['./doc-editor.component.scss']
 })
-export class DocEditorComponent implements OnInit {
+export class DocEditorComponent {
+  openToolWindow(e: any) {
+    var allOpenEl = document.getElementsByClassName('open');
+    if (allOpenEl.length > 0) {
+      for (var i = 0; i < allOpenEl.length; i++) {
+        if (allOpenEl[i].id !== e) {
+          allOpenEl[i].classList.remove('open');
+        }
+      }
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
+    var element = document.getElementById(e);
+    if (element.classList.contains('open')) {
+      element.classList.remove('open');
+    } else {
+      element.classList.add('open');
+    }
   }
-
 }
