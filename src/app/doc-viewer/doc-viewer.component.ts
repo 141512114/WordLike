fc67e20682215c41e38f12c98bd247b9635b3494
Nolan Schiemann <derjuniorschiemann@gmail.com>
2020-12-08 21:22:12 +0100

    initial-commit

--- src/app/doc-viewer/doc-viewer.component.ts
+++ src/app/doc-viewer/doc-viewer.component.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+function getSelectionText() {
+  var text = ""; if (window.getSelection) {text = window.getSelection().toString();}
+  return text;
+}
+
 @Component({
   selector: 'app-doc-viewer',
   templateUrl: './doc-viewer.component.html',
   styleUrls: ['./doc-viewer.component.scss']
 })
 export class DocViewerComponent implements OnInit {
-  ngOnInit(): void {
+  ngOnInit() {
+    var isDown = false, docContainer = document.getElementById('document-container');
+    if (docContainer != null) {
+      var docContainerChildren = docContainer.children.length;
+      for (var i = 0; i < docContainerChildren; i++) {
+        docContainer.children[i].addEventListener('mousedown', function() {isDown = true;});
+        docContainer.children[i].addEventListener('mouseup', function() {
+          isDown = false;
+          var selection = getSelectionText(); if (selection !== "") {
+            console.log(selection);
+            var range; if (window.getSelection && window.getSelection().getRangeAt) {
+              range = window.getSelection().getRangeAt(0);
+              range.deleteContents();
+              var wrapper = document.createElement("div");
+              wrapper.innerHTML = "<b>"+selection+"</b>";
+              var frag = document.createDocumentFragment(), child;
+              while (child = wrapper.firstChild) {frag.appendChild(child);}
+              range.insertNode(frag);
+            }
+          }
+        });
+      }
+    }
   }
 }
