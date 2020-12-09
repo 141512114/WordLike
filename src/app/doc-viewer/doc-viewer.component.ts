f9e1170bf3b8f629c4f478bd91fc5fced61b1331
Nolan Schiemann <derjuniorschiemann@gmail.com>
2020-12-09 03:33:21 +0100

    initial-commit

--- src/app/doc-viewer/doc-viewer.component.ts
+++ src/app/doc-viewer/doc-viewer.component.ts
@@ -1,27 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
-var selectionPart = null;
+var selectionEl = null, selectionHTMLEl = null, selectionText = null, range = null;
 
 function getSelectionText() {
   var text = ""; if (window.getSelection) {text = window.getSelection().toString();}
   return text;
 }
 
-function executeTool(tool: string, editPart: any) {
-  switch (tool) {
-    case 'color-tool':
-      editPart.style.color = '#000000';
-
-      break;
-
-    case 'font-change-tool':
-      break;
-
-    case 'font-size-tool':
-      break;
-
-    default:
-      break;
+function executeTool(tool: string) {
+  if (selectionEl !== null && selectionHTMLEl !== null && selectionText !== null && range !== null) {
+    switch (tool) {
+      case 'color-tool':
+        range.deleteContents();
+        var wrapper = document.createElement("div");
+        var frag = document.createDocumentFragment(), child;
+  
+        if (selectionHTMLEl.tagName === "SPAN" && selectionHTMLEl.innerText === selectionText) {
+          let span_item = selectionHTMLEl;
+          span_item.style.color = (document.getElementById('color-code') as HTMLInputElement).value;
+          wrapper.appendChild(span_item);
+        } else {
+          let span_item = document.createElement("span");
+          span_item.innerHTML = selectionText;
+          span_item.style.color = (document.getElementById('color-code') as HTMLInputElement).value;
+          wrapper.appendChild(span_item);
+        }
+        
+  
+        while (child = wrapper.firstChild) {frag.appendChild(child);}
+        range.insertNode(frag);
+        break;
+  
+      case 'font-change-tool':
+        break;
+  
+      case 'font-size-tool':
+        break;
+  
+      default:
+        break;
+    }
   }
 }
 
@@ -39,16 +57,12 @@ export class DocViewerComponent implements OnInit {
         docContainer.children[i].addEventListener('mousedown', function() {isDown = true;});
         docContainer.children[i].addEventListener('mouseup', function() {
           if (isDown) {
-            var selection = getSelectionText(); if (selection !== "") {
-              var range; if (window.getSelection && window.getSelection().getRangeAt) {
-                range = window.getSelection().getRangeAt(0);
-                range.deleteContents();
-                var wrapper = document.createElement("div");
-                wrapper.innerHTML = "<span>"+selection+"</span>";
-                var frag = document.createDocumentFragment(), child;
-                while (child = wrapper.firstChild) {frag.appendChild(child);}
-                range.insertNode(frag);
-                selectionPart = window.getSelection().focusNode;
+            selectionText = getSelectionText(); if (selectionText !== "") {
+              if (window.getSelection && window.getSelection().getRangeAt) {
+                selectionEl = window.getSelection();
+                selectionHTMLEl = selectionEl.baseNode.parentNode;
+                console.log(selectionEl);
+                range = selectionEl.getRangeAt(0);
               }
             }
             isDown = false;
@@ -56,10 +70,19 @@ export class DocViewerComponent implements OnInit {
         });
       }
     }
+
+    const colorIcon = document.getElementById('color-icon');
+    const colorCode = document.getElementById('color-code') as HTMLInputElement;
+    colorCode.addEventListener('keydown', function onEvent(e:KeyboardEvent) {
+      if (e.keyCode === 13) {
+        colorIcon.style.backgroundColor = colorCode.value;
+        executeTool('color-tool');
+      }
+    });
   }
 
   openToolWindow(tool: string) {
-    var allOpenEl = document.getElementsByClassName('open');
+    const allOpenEl = document.getElementsByClassName('open');
     if (allOpenEl.length > 0) {
       for (var i = 0; i < allOpenEl.length; i++) {
         if (allOpenEl[i].id !== tool) {
@@ -68,12 +91,12 @@ export class DocViewerComponent implements OnInit {
       }
     }
 
-    var element = document.getElementById(tool);
+    const element = document.getElementById(tool);
     if (element.classList.contains('open')) {
       element.classList.remove('open');
     } else {
       element.classList.add('open');
-      executeTool(tool, selectionPart);
+      executeTool(tool);
     }
   }
 }
